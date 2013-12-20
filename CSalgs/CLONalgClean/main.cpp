d21112ff3f8c7338cf3adb64f8d6bf329838dce7
alojzije <shljunak@hotmail.com>
2013-12-20 01:27:10 +0100

    Add new average stats for the ProportionalCloning CLONALG2 version of the algorithm ( statsProportionalCloningCLONALG2.odt), also refresh the main file

--- CSalgs/CLONalgClean/main.cpp
+++ CSalgs/CLONalgClean/main.cpp
@@ -3,13 +3,16 @@
 /**
  * \brief Clonal Selection Algorithm (see e.g. http://en.wikipedia.org/wiki/Clonal_Selection_Algorithm)
  * 
- * this CLONALG implements: - static cloning :  n of the best antibodies are cloned beta time, making the size of the clones population  equal n*beta
- *							- proportional cloning: number of clones per antibody is proportional to that ab's fitness
- *							- inversely proportional hypermutation : better antibodies are mutated less
- *							- CLONALG1 - at new generation each antibody will be substituded by the best individual of its set of beta*population clones
- *							- CLONALG2 - new generation will be formed by the best (1-d)*populationSize clones ( or all if the number of clones is less than that )- birthPhase where d * populationSize of new antibodies are randomly created and added to the population
+ * this CLONALG implements: 
+ *			- cloning Versions:	- static cloning :  n of the best antibodies are cloned beta*populationSize times
+ *								- proportional cloning: number of clones per antibody is proportional to that ab's fitness
+ *			- inversely proportional hypermutation : better antibodies are mutated less
+ *			- selectionSchemes:	- CLONALG1 - at new generation each antibody will be substituded by the best individual of its set of beta*population clones
+ *								- CLONALG2 - new generation will be formed by the best (1-d)*populationSize clones ( or all if the number of clones is less than that )
+ *          - birthPhase: where d * populationSize of new antibodies are randomly created and added to the population for diversification
  *							
- * CLONALG algorithm accepts only a single FloatingPoint genotype (vector of real values).
+ * CLONALG algorithm accepts only a single FloatingPoint genotype
+ * Additionally, if chosen, selectionScheme CLONALG2 adds a FloatingPoint genotype  (parentAntibody) to mark which clone came from which antibods 
  */
 class MyAlg : public Algorithm
 {
